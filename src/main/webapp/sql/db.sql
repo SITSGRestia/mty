ed6cec64ae73acc5cff675779222c531d78e14ea
zdh <zdhhehahe@163.com>
2017-09-01 15:23:21 +0800

    fore-end commit 9/1 15:23

---
+++ src/main/webapp/sql/db.sql
@@ -0,0 +1,137 @@
+CREATE TABLE IF NOT EXISTS users(
+id INT NOT NULL,
+username VARCHAR(100) NULL,
+password VARCHAR(100) NULL,
+password_salt VARCHAR(100) NULL,
+PRIMARY KEY(
+`id`
+))
+ENGINE
+ =
+InnoDB
+CREATE
+TABLE
+IF
+NOT
+EXISTS
+`shiro`
+.
+`user_roles`
+ (
+`role_id`
+INT
+NOT
+NULL
+,
+`role_name`
+VARCHAR
+(
+100
+)
+NULL
+,
+`users_id`
+INT
+NOT
+NULL
+,
+PRIMARY
+KEY
+ (
+`role_id`
+),
+INDEX
+`fk_user_roles_users1_idx`
+ (
+`users_id`
+ASC
+),
+CONSTRAINT
+`fk_user_roles_users1`
+FOREIGN
+KEY
+ (
+`users_id`
+)
+REFERENCES
+`shiro`
+.
+`users`
+ (
+`id`
+)
+ON
+DELETE
+NO
+ACTION
+ON
+UPDATE
+NO
+ACTION
+)
+ENGINE
+ =
+InnoDB
+CREATE
+TABLE
+IF
+NOT
+EXISTS
+`shiro`
+.
+`roles_permissions`
+ (
+`permission`
+VARCHAR
+(
+100
+)
+NULL
+,
+`p_id`
+INT
+NOT
+NULL
+,
+`user_roles_role_id`
+INT
+NOT
+NULL
+,
+PRIMARY
+KEY
+ (
+`p_id`
+),
+INDEX
+`fk_roles_permissions_user_roles1_idx`
+ (
+`user_roles_role_id`
+ASC
+),
+CONSTRAINT
+`fk_roles_permissions_user_roles1`
+FOREIGN
+KEY
+ (
+`user_roles_role_id`
+)
+REFERENCES
+`shiro`
+.
+`user_roles`
+ (
+`role_id`
+)
+ON
+DELETE
+NO
+ACTION
+ON
+UPDATE
+NO
+ACTION
+)
+ENGINE
+ =
+InnoDB
